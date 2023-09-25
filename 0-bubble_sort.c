a52b4a5b697236039947315b3af4df4097b09bc7
Yahya Achrane <yahya.achrane15@gmail.com>
2023-09-25 22:42:17 +0100

    Bubble Sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting function/algo
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp;
+
+    if (array == NULL || size < 2)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+        for (j = 0; j < size - 1 - i; j++)
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+                print_array(array, size);
+            }
+}
